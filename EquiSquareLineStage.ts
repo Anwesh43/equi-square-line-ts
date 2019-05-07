d20c55359a5251735769bea5ae93267101a568ea
Anwesh43 <anweshthecool0@gmail.com>
2019-05-07 22:34:21 +0530

    adding constants

---
+++ EquiSquareLineStage.ts
@@ -0,0 +1,11 @@
+const scGap : number = 0.05
+const scDiv : number = 0.51
+const nodes : number = 5
+const lines : number = 2
+const parts : number = 2
+const strokeFactor : number = 90
+const sizeFactor : number = 2.9
+const foreColor : string = "#673AB7"
+const backColor : string = "#BDBDBD"
+const w : number = window.innerWidth
+const h : number = window.innerHeight
