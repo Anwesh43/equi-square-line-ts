da9d77a23757936a56c72aeece57abdfaefcd76e
Anwesh43 <anweshthecool0@gmail.com>
2019-05-07 23:47:53 +0530

    adding EquiSquareLine class

--- EquiSquareLineStage.ts
+++ EquiSquareLineStage.ts
@@ -189,3 +189,27 @@ class ESLNode {
         return this
     }
 }
+
+class EquiSquareLine {
+
+    root : ESLNode = new ESLNode(0)
+    curr : ESLNode = this.root
+    dir : number = 1
+    
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
