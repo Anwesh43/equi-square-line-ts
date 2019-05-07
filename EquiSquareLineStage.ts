d181605fc2523ba133ffe0b5b2ad64ff9da01e3e
Anwesh43 <anweshthecool0@gmail.com>
2019-05-07 23:32:51 +0530

    adding EquiSquareLineStage class

--- EquiSquareLineStage.ts
+++ EquiSquareLineStage.ts
@@ -69,3 +69,34 @@ class DrawingUtil {
         context.restore()
     }
 }
+
+class EquiSquareLineStage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : EquiSquareLineStage = new EquiSquareLineStage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
